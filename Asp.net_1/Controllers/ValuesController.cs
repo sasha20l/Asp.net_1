21a7e4da8d17ad2d3de214abf65c929c8c647d9c
Sasha21l <sasha21l@mail.ru>
2021-03-23 07:32:45 +0500

    Добавьте файлы проекта.

---
+++ Asp.net_1/Controllers/ValuesController.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Asp.net_1.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        private readonly ValuesHolder holder;
+
+        public ValuesController(ValuesHolder holder)
+        {
+            this.holder = holder;
+        }
+
+        [HttpPost("create")]
+        public IActionResult Create([FromQuery] string input)
+        {
+            holder.Values.Add(input);
+            return Ok();
+        }
+
+        [HttpGet("read")]
+        public IActionResult Read()
+        {
+            return Ok(holder.Values);
+        }
+
+        [HttpPut("update")]
+        public IActionResult Update([FromQuery] string stringsToUpdate, [FromQuery] string newValue)
+        {
+            for (int i = 0; i < holder.Values.Count; i++)
+            {
+                if (holder.Values[i] == stringsToUpdate)
+                    holder.Values[i] = newValue;
+            }
+
+            return Ok();
+        }
+
+        [HttpDelete("delete")]
+        public IActionResult Delete([FromQuery] string stringsToDelete)
+        {
+            holder.Values = holder.Values.Where(w => w != stringsToDelete).ToList();
+            return Ok();
+        }
+    }
+   
+}
+
